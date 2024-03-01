90ff78aa2235a24255fcf5062a30afd7e9d0c957
Azael Dahaka <azaeldahaka.v@gmail.com>
2024-03-01 16:20:58 -0300

    Agregando mas funciones a func.py

--- func.py
+++ func.py
@@ -1,10 +1,7 @@
 #Se inicializan las variables y se piden los datos requeridos
-a = int(input("ingresar un numero: "))
-b = int(input("ingresar otro numero: "))
-
-
-
-
+a = int(input("\n\tingresar un numero: "))
+b = int(input("\tingresar otro numero: "))
 
 #Se muestra por pantalla la operacion realizada.
 print(f"Suma={a+b}")
+print("Gracias por usar nuestro codigo :)")
