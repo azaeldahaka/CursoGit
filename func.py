1f6de1a0374837e61519ec1e96cb7b52a8d8b60b
Azael Dahaka <azaeldahaka.v@gmail.com>
2024-03-01 16:03:24 -0300

    Arreglo de error de int

--- func.py
+++ func.py
@@ -1,4 +1,6 @@
-a = input(int("ingresar un numero: "))
-b = input(int("ingresar otro numero: "))
+#Se inicializan las variables y se piden los datos requeridos
+a = int(input("ingresar un numero: "))
+b = int(input("ingresar otro numero: "))
 
+#Se muestra por pantalla la operacion realizada.
 print(f"Suma={a+b}")
